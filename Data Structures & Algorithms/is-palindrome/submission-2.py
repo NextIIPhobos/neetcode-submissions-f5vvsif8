f0ba4b28607a538f277bd1972260195bdcbc50cf
NikitoS. <crossfire.akk12@gmail.com>
2026-03-31 09:43:40 +0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        length = len(s)
+        l_i = 0
+        r_i= length - 1 
+        flag = True
+        while l_i < length and r_i >= 0 and l_i != r_i:
+            while l_i < length and not s[l_i].lower().isalnum():
+                l_i += 1
+            while r_i >= 0 and not s[r_i].lower().isalnum():
+                r_i -= 1
+            if l_i < length and r_i >= 0:
+                if s[l_i].lower() == s[r_i].lower():
+                    flag = True
+                else:
+                    flag = False
+                    break
+            else: return flag
+            l_i += 1
+            r_i -= 1
+        return flag
+            
+            
