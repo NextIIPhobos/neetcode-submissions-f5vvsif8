7147cd9f4feb96238afaac63e3ffc5d8af84a4a3
NikitoS. <crossfire.akk12@gmail.com>
2026-04-04 18:44:38 +0400

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        ans, sol = [], []
+
+        def backtrack():
+            if len(sol) == n:
+                ans.append(sol.copy())
+                return
+
+            for x in nums:
+                if x not in sol:
+                    sol.append(x)
+                    backtrack()
+                    sol.pop()
+        backtrack()
+        return ans
+
+            
+
+
+        
